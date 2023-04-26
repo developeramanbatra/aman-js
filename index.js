0e58029af1385df6de2075be7a0e1ef7e9d7b04c
Tushar Kumar <tusharkumar@192.168.1.9>
2023-04-26 15:58:47 +0530

    [NEW] added the nodejs to system

---
+++ index.js
@@ -0,0 +1,34 @@
+// problem - we cannot run the javascript inside the system or compiler
+// (single line comment) node js  - it runs javascript inside the system or compiler
+/* 
+    (multi line comment)
+
+    1. index.js (standard) (done)
+    2. npm init (initialize the folder for nodejs) - need to check the options
+    3. node index.js (just run like this)
+    4. Adding script -  "scripts": {
+                            "test": "echo \"Error: no test specified\" && exit 1",
+                            "start": "node index.js"
+                        },
+    5. type inside the folder - npm (node package manager) start 
+
+    options - 
+        package name: (aman) 
+        version: (1.0.0) 
+        description: this is the aman batra application 
+        entry point: (index.js) 
+        test command: 
+        git repository: (https://github.com/developeramanbatra/aman-js.git) 
+        keywords: fun project
+        author: Aman Batra
+        license: (ISC) 
+
+
+
+*/
+
+
+var amanvar = 'aman';
+
+console.log('aman is the text => ', amanvar);
+
